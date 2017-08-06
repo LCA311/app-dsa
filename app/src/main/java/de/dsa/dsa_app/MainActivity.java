950d81293d34aeb8279c636cc4d63333ec4ea779
LCA311 <spitzer-webdesign@outlook.de>
2017-08-06 17:29:59 +0200

    Initial commit

---
+++ app/src/main/java/de/dsa/dsa_app/MainActivity.java
@@ -0,0 +1,15 @@
+package de.dsa.dsa_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle b) {
+
+        super.onCreate(b);
+
+    }
+
+}
